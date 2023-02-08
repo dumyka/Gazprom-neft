6bd5c1c3a4caa5ed20787dc4a50dbc7e8b0c77de
dumyka <dumyka1990@gmail.com>
2023-02-08 19:05:05 +0300

    commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,6 +12,7 @@ public class TestBase {
   @BeforeAll
   static void beforeAll() {
     Configuration.browserSize = "1920x1080";
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     //Configuration.holdBrowserOpen = true;
   }
 }
